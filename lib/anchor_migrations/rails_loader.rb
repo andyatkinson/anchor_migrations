ccac5caaf6763dbcbec1eb06ac2743e941f4cd91
Andrew Atkinson <andyatkinson@gmail.com>
2025-06-23 13:39:41 -0500

    Updates

--- lib/anchor_migrations/rails_loader.rb
+++ lib/anchor_migrations/rails_loader.rb
@@ -4,8 +4,6 @@ module AnchorMigrations
   # Load the Rails environment
   module RailsLoader
     def self.load_rails!
-      return unless defined?(Rails) && defined?(Rails.application)
-
       env_path = File.expand_path("config/environment", Dir.pwd)
       require "#{env_path}.rb" if File.exist?("#{env_path}.rb")
     end
