3c985f34bd1b03032ac9b2d92029d80592bec149
Andrew Atkinson <andyatkinson@gmail.com>
2025-06-22 23:07:41 -0500

    Rubocop

--- lib/anchor_migrations/cli.rb
+++ lib/anchor_migrations/cli.rb
@@ -56,9 +56,7 @@ module AnchorMigrations
 
     def lint
       # TODO: return unless there's a directory ./anchor_migrations
-      unless system("which squawk > /dev/null 2>&1")
-        abort "Error: 'squawk' command not found in PATH."
-      end
+      abort "Error: 'squawk' command not found in PATH." unless system("which squawk > /dev/null 2>&1")
       system("squawk lint anchor_migrations/*.sql")
     end
 
