215df826a58bafb0b014bffd4de3f5b149ad0ac7
Andrew Atkinson <andyatkinson@gmail.com>
2025-06-22 22:59:48 -0500

    Disable tests for now

--- test/test_anchor_migrations.rb
+++ test/test_anchor_migrations.rb
@@ -7,7 +7,7 @@ class TestAnchorMigrations < Minitest::Test
     refute_nil ::AnchorMigrations::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_it_does_something_useful
+  #   assert false
+  # end
 end
